9a93aa1a6b3e947498cd5db85f02b57a47409eb5
Feng Zhou <fenzhou@microsoft.com>
2020-03-17 13:52:51 +0800

    add retry for package verify

--- scripts/release/debian/verify_debian_in_docker.sh
+++ scripts/release/debian/verify_debian_in_docker.sh
@@ -4,14 +4,29 @@
 
 apt update -y
 apt install curl -y
-curl -sL https://aka.ms/InstallAzureCLIDeb | bash 
-ACTUAL_VERSION=$(az version | sed -n 's|"azure-cli": "\(.*\)",|\1|p' | sed 's|[[:space:]]||g')
-echo "actual version:${ACTUAL_VERSION}"
-echo "expected version:${CLI_VERSION}"
+counter=4
 
-if [ "$ACTUAL_VERSION" != "$CLI_VERSION" ]; then
-    echo "Latest package is not in the repo."
-    exit 1
-else
-    echo "Latest package is verified."
-fi
+while [ $counter -gt 0 ]
+do
+    curl -sL https://aka.ms/InstallAzureCLIDeb | bash 
+    ACTUAL_VERSION=$(az version | sed -n 's|"azure-cli": "\(.*\)",|\1|p' | sed 's|[[:space:]]||g')
+    echo "actual version:${ACTUAL_VERSION}"
+    echo "expected version:${CLI_VERSION}"
+
+    if [ "$ACTUAL_VERSION" != "$CLI_VERSION" ]; then
+        if [ ! -z "$ACTUAL_VERSION" ]; then
+            echo "Latest package is not in the repo."
+            exit 1
+        fi
+        echo "wait 5m"
+        sleep 300
+        counter=$(( $counter - 1 ))
+    else
+        echo "Latest package is verified."
+        exit 0
+    fi
+done
+echo "Timeout!"
+exit 1
+
+
--- scripts/release/rpm/verify_rpm_in_docker.sh
+++ scripts/release/rpm/verify_rpm_in_docker.sh
@@ -10,15 +10,27 @@ enabled=1
 gpgcheck=1
 gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
 
-yum install azure-cli -y
+counter=4
 
-ACTUAL_VERSION=$(az version | sed -n 's|"azure-cli": "\(.*\)",|\1|p' | sed 's|[[:space:]]||g')
-echo "actual version:${ACTUAL_VERSION}"
-echo "expected version:${CLI_VERSION}"
+while [ $counter -gt 0 ]
+do
+    yum install azure-cli -y
+    ACTUAL_VERSION=$(az version | sed -n 's|"azure-cli": "\(.*\)",|\1|p' | sed 's|[[:space:]]||g')
+    echo "actual version:${ACTUAL_VERSION}"
+    echo "expected version:${CLI_VERSION}"
 
-if [ "$ACTUAL_VERSION" != "$CLI_VERSION" ]; then
-    echo "Latest package is not in the repo."
-    exit 1
-else
-    echo "Latest package is verified."
-fi
+    if [ "$ACTUAL_VERSION" != "$CLI_VERSION" ]; then
+        if [ ! -z "$ACTUAL_VERSION" ]; then
+            echo "Latest package is not in the repo."
+            exit 1
+        fi
+        echo "wait 5m"
+        sleep 300
+        counter=$(( $counter - 1 ))
+    else
+        echo "Latest package is verified."
+        exit 0
+    fi
+done
+echo "Timeout!"
+exit 1
